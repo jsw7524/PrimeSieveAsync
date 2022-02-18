6b790cdc2f94edcc2497786bc6bfdb4c40e63f2d
jsw7524 <jsw7524@gmail.com>
2022-02-18 22:10:20 +0800

    MultiAwait test

--- PrimeSieveAsync/Program.cs
+++ PrimeSieveAsync/Program.cs
@@ -55,6 +55,23 @@ namespace MyApp // Note: actual namespace depends on the project name.
         }
     }
 
+    public class MultiAwait
+    {
+        public async void run()
+        {
+            List<Task> tasks = new List<Task>();
+
+            tasks.Add(Task.Run(() => { Thread.Sleep(3000); Console.WriteLine("A"); }));
+            tasks.Add(Task.Run(() => { Thread.Sleep(5000); Console.WriteLine("B"); }));
+            tasks.Add(Task.Run(() => { Thread.Sleep(1000); Console.WriteLine("C"); }));
+
+            foreach (Task task in tasks)
+            {
+                await task;
+            }
+        }
+    }
+
 
     internal class Program
     {
@@ -63,6 +80,10 @@ namespace MyApp // Note: actual namespace depends on the project name.
             PrimeSieve ps = new PrimeSieve(500000000);
             ps.Run();
             Console.WriteLine("Hello World!");
+            //MultiAwait multiAwait = new MultiAwait();
+            //multiAwait.run();
+            //Console.WriteLine("Hello World!");
+            //while (true) ;
         }
     }
 }
